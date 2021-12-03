5a9c14370abd9c7c43d8bc9aafa35703da22cbe9
fralabi <66752210+fralabi@users.noreply.github.com>
2021-12-03 14:38:08 +0100

    Update Hello World.c

--- Primo_Anno/CALCOLATORI ELETTRONICI C.I. - FONDAMENTI DI PROGRAMMAZIONE/ESERCITAZIONE/1/Hello World.c
+++ Primo_Anno/CALCOLATORI ELETTRONICI C.I. - FONDAMENTI DI PROGRAMMAZIONE/ESERCITAZIONE/1/Hello World.c
@@ -1,4 +1,11 @@
-//Stampare "Hello world!" a schermo
+// Author: Francesco La Barbera
+//	Date 19/11/2021
+//
+//  Aprire il file "Hello World.c" e scrivere un programma in C che stampi a schermo la stringa “Hello World!”
+//
+//  Esempio
+//      Hello world!
+
 #include <stdio.h>
 
 int main(void){
