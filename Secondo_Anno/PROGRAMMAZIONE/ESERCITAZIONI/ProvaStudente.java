58a36821f5a6cfca79012a9ca63028cf0a9df3c1
La Barbera Francesco <66752210+fralabi@users.noreply.github.com>
2022-11-07 12:22:15 +0100

    Update ProvaStudente.java

--- Secondo_Anno/PROGRAMMAZIONE/ESERCITAZIONI/ProvaStudente.java
+++ Secondo_Anno/PROGRAMMAZIONE/ESERCITAZIONI/ProvaStudente.java
@@ -2,7 +2,7 @@ public class ProvaStudente {
     
     public static void main(String[] args) {
         Studente stud1 = new Studente("Richard","Stallman", 12, 11, 1957, 0456372);
-        Studente stud2 = new Studente("Massimiliano","Jobs", 24, 2, 1934, 0456373);
+        Studente stud2 = new Studente("Steve","Jobs", 24, 2, 1955, 0456373);
 
         System.out.println(stud1);
         System.out.println(stud2);
